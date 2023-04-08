c1b33a78414be63b513baac543a133c3adae7fd4
kunjan5228 <61596879+kunjan5228@users.noreply.github.com>
2023-04-07 22:23:53 -0400

    NEw Employee Class Added

---
+++ GitHubCSharpDemo27May/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubCSharpDemo27May
+{
+    class Employee
+    {
+        public void EmployeeDetails()
+        {
+            Console.WriteLine("This is employee class");
+        }
+    }
+}
